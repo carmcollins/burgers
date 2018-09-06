63a95c1f2ff413f5bdba7ab6a9685f13433a9797
Carmen Collins <hellocarmencollins@gmail.com>
2018-09-05 19:39:58 -0500

    controller setup

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,53 @@
+var express = require("express");
+
+var router = express.Router();
+
+var burger = require("../models/burger.js");
+
+// Create all routes and set up logic within routes where required
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+  
+router.post("/api/burgers", function(req, res) {
+    burger.create(["name"], [req.body.name], function(result) {
+        res.json({ id: result.insertId });
+    });
+});
+  
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
+    burger.update({
+      devoured: req.body.devoured
+    }, condition, function(result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+  
+router.delete("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    burger.delete(condition, function(result) {
+        if (result.affectedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        } 
+    });
+});
+  
+// Export routes for server to use
+module.exports = router;
