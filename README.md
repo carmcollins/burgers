ebae4425134cce5016b0318396e139ad9af9d51e
Carmen Collins <hellocarmencollins@gmail.com>
2018-09-06 18:47:08 -0500

    creating readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# burgers
+# Burgers, Burgers, Burgers!
+
+### About This Project
+This project uses Node, Express, Handlebars, MySQL, and a homemade ORM to create a burger ordering game. Users can input the names of burgers they'd like to eat and the app will display the burger on the "On The Menu" card. Then, users can select which burgers they'd like to order and the app will move the burger to the "Your Order" card. Each burger is stored in the MySQL database, whether it is ordered or not.
+
+### To Play The Game
+Go to [this page]() and add a burger to begin!
+
+<img src="public/assets/images/app.png">
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,6 @@
-INSERT INTO burgers (burger_name) VALUES ("Cheeseburger");
-INSERT INTO burgers (burger_name) VALUES ("Turkey Burger");
-INSERT INTO burgers (burger_name) VALUES ("Black Bean Burger");
+INSERT INTO burgers (burger_name) VALUES ("Poblano Picasso Burger");
+INSERT INTO burgers (burger_name) VALUES ("Sweet Home Avocado Burger");
+INSERT INTO burgers (burger_name) VALUES ("Say It Ain’t Cilantro Burger");
+INSERT INTO burgers (burger_name) VALUES ("I Heartichoke You Burger");
+INSERT INTO burgers (burger_name) VALUES ("Summer Thyme Burger");
+INSERT INTO burgers (burger_name) VALUES ("I Mint To Do That Burger");
